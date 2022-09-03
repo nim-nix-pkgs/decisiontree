80ee37130a5d99f47f43ec873aa8419c15942150
hugosenari <hugosenari@gmail.com>
2022-09-03 19:47:31 -0300

    feat(nimpkgs): update lock

--- source/src/hyperparams.nim
+++
@@ -1,8 +0,0 @@
-type Hyperparams* = tuple[max_depth: int, min_samples_split: int, max_features: float32, min_impurity_decrease: float32, bagging: float32]
-
-template hyperparams_binding*(T: typed) =
-    proc bagging*(t: T): auto {.inline.} = t.hyperparams.bagging
-    proc max_depth*(t: T): auto {.inline.} = t.hyperparams.max_depth
-    proc min_samples_split*(t: T): auto {.inline.} = t.hyperparams.min_samples_split
-    proc min_impurity_decrease*(t: T): auto {.inline.} = t.hyperparams.min_impurity_decrease
-    proc max_features*(t: T): auto {.inline.} = t.hyperparams.max_features
--- source/src/task.nim
+++
@@ -1,2 +0,0 @@
-type Task* = enum 
-    Classification, Regression
